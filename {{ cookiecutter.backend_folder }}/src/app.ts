b24e99bb307061b906e016e5e954b4cdf33c77b7
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-03-07 18:08:12 -0700

    added a bunch of new boilerplate

--- {{ cookiecutter.backend_folder }}/src/app.ts
+++ {{ cookiecutter.backend_folder }}/src/app.ts
@@ -1,9 +1,22 @@
+require('dotenv').config()
 import express from 'express';
+import cors from 'cors'
 
 import * as redis from './redis'
 import * as db from './db'
 
 const app = express()
+
+// Body Parsing
+app.use(express.json());
+
+// Cors
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+app.use(cors({ origin: ['localhost:3000',] }))
 
 app.get('/', (req: express.Request, res: express.Response) => {
   res.send('pong!')
